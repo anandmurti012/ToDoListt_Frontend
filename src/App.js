7e368c0392d025a93c93c64d343d10174647b6db
anandmurti012 <anand.raj431@gmail.com>
2023-10-08 00:20:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
+
+import { useEffect, useState } from "react";
+import ToDo from "./components/ToDo";
+import { getAllToDo,addToDo,updateTodo, deleteToDo } from "./utils/HandleApi";
+
 
 function App() {
+  const [toDo, setToDo] = useState([])
+  const [text, setText] = useState("")
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [toDoId, setToDoId] = useState(false)
+  useEffect(() => {
+    getAllToDo(setToDo)
+  }, [])
+
+  const updateMode = (_id, text) =>{
+    setIsUpdating(true)
+    setText(text)
+    setToDoId(_id)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1>Todo App</h1>
+        <div className='top'>
+          <input type='text' placeholder='Add ToDos...' value={text} onChange={(e) => setText(e.target.value)} />
+          <div className="add" onClick={isUpdating ? () => updateTodo(toDoId, text, setToDo, setText, setIsUpdating) : () => addToDo(text, setText, setToDo)}>{isUpdating ? "Update" : "Add"}</div>
+        </div>
+
+        <div className="list">
+
+          {toDo.map((item) => <ToDo key={item._id} text={item.text} updateMode={() => updateMode(item._id, item.text)} deleteToDo = {() => deleteToDo(item._id, setToDo)}/>)}
+          {/* Calling ToDo */}
+
+          {/* <ToDo text="Hii There" />
+          <ToDo text="Hii There" />
+          <ToDo text="Hii There" /> */}
+
+        </div>
+      </div>
     </div>
   );
 }
